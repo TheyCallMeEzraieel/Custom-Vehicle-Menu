bd0aed02473d8d9fdd4ee57b186d7193a5129d94
TheyCallMeEzraieel <80192196+TheyCallMeEzraieel@users.noreply.github.com>
2024-09-02 14:30:55 +0330

    Update config

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 ConfigMenu = Config or {}
 
-ConfigMenu.MenuLocation = 'centerleft' -- e.g. topleft | topcenter | topright | centerleft | center | centerright | bottomleft | bottomcenter | bottomright
+ConfigMenu.MenuLocation = 'topright' -- e.g. topleft | topcenter | topright | centerleft | center | centerright | bottomleft | bottomcenter | bottomright
 ConfigMenu.MenuSize = 'size-125'
 
